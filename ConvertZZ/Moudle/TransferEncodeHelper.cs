e9b66ad7714991ceb9bafba67282bc34b68dceb4
Flier <pow7000@yahoo.com.tw>
2018-07-25 15:45:30 +0800

    清理沒用的程式碼

--- ConvertZZ/Moudle/TransferEncodeHelper.cs
+++ ConvertZZ/Moudle/TransferEncodeHelper.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ConvertZZ
 {
@@ -12,28 +9,13 @@ namespace ConvertZZ
         {
             try
             {
-                /*
-                byte[] c = originalEncode.GetBytes(str);
-               byte[] d = Encoding.Convert(originalEncode, targetEncode, c);
-                return targetEncode.GetString(d);
-                */
-                /*
-                byte[] c = originalEncode.GetBytes(str);
-                return targetEncode.GetString(c);
-                */
-                //str = Encoding.Convert(Encoding.UTF8, originalEncode, str);
                 byte[] temp = originalEncode.GetBytes(str);
                 byte[] targetEncodeBytes = Encoding.Convert(originalEncode, targetEncode, temp);
-
-
 
                 char[] targetEncodeChars = new char[targetEncode.GetCharCount(targetEncodeBytes, 0, targetEncodeBytes.Length)];
                 targetEncode.GetChars(targetEncodeBytes, 0, targetEncodeBytes.Length, targetEncodeChars, 0);
                 string targetEncodeString = new string(targetEncodeChars);
                 return targetEncodeString;
-
-
-                //return targetEncode.GetString(targetEncodeBytes);
             }
             catch
             {

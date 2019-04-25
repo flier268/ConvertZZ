2270b867b76fa1fb8f7f03e323c8c4a4a89651a8
flier268 <flier268@gmail.com>
2019-04-25 14:00:32 +0800

    Window_DictionaryEditor改為Show()

--- ConvertZZ/Window_Setting.xaml.cs
+++ ConvertZZ/Window_Setting.xaml.cs
@@ -191,7 +191,7 @@ namespace ConvertZZ
 
         private void Button_DictionaryEdit_Click(object sender, RoutedEventArgs e)
         {
-            new Window_DictionaryEditor().ShowDialog();
+            new Window_DictionaryEditor().Show();
         }
 
         public KeyValuePair<string, string> Quick_R2 { get => _Quick_R2; set { _Quick_R2 = value; OnPropertyChanged(); SaveSetting(); } }

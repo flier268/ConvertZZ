88c8ac4e820fef6359a4a2adbee7d0c1411e10bc
flier268 <flier268@gmail.com>
2019-05-31 14:10:54 +0800

    改用Visual Studio 2019

--- ConvertZZ/Properties/Resources.Designer.cs
+++ ConvertZZ/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace ConvertZZ.Properties {
     // 類別透過 ResGen 或 Visual Studio 這類工具。
     // 若要加入或移除成員，請編輯您的 .ResX 檔，然後重新執行 ResGen
     // (利用 /str 選項)，或重建您的 VS 專案。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {

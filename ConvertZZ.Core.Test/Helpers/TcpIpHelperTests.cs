62639fa0919dbb44067743782dc605214a321415
flier268 <flier268@gmail.com>
2022-06-29 12:25:56 +0800

    unittest: ConvertZZ.Core.TcpIpHelper

---
+++ ConvertZZ.Core.Test/Helpers/TcpIpHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ConvertZZ.Core.Test.Helpers
+{
+    [TestFixture]
+    public class TcpIpHelperTests
+    {
+        [Test]
+        public void GetAllProcesses_StateUnderTest_ExpectedBehavior()
+        {
+            var listener = new TcpListener(IPAddress.Any, 0);
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+
+            // Act
+            var result = TcpIpHelper.GetAllProcesses();
+            listener.Stop();
+            Assert.True(result.Any(x => x.Port == port));
+        }
+
+        [Test]
+        public void GetRandomUnusedPort_StateUnderTest_ExpectedBehavior()
+        {
+            // Act
+            var result = TcpIpHelper.GetRandomUnusedPort();
+
+            Assert.DoesNotThrow(() =>
+            {
+                var listener = new TcpListener(IPAddress.Any, result);
+                listener.Start();
+                listener.Stop();
+            });
+        }
+    }
+}

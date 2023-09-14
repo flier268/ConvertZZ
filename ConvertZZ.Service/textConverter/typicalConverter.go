5cb5e3d50e9094e15481113da57b4f31e0492a46
flier268 <flier268@gmail.com>
2023-09-14 17:41:55 +0800

    rename: typicalConverter.converter to openCC_Converter

--- ConvertZZ.Service/textConverter/typicalConverter.go
+++ ConvertZZ.Service/textConverter/typicalConverter.go
@@ -9,34 +9,37 @@ import (
 )
 
 type typicalConverter struct {
-	option    Options
-	converter *opencc.OpenCC
-	segmenter *gse.Segmenter
+	option           Options
+	openCC_Converter *opencc.OpenCC
+	segmenter        *gse.Segmenter
 }
 
+// Init 初始化
+// option: 轉換的選項
+// segmenter: 分詞器(選填，nul則會用預設分詞)
 func (converter *typicalConverter) Init(option Options, segmenter ...*gse.Segmenter) {
 	var err error
 	switch option {
 	case S2T:
-		converter.converter, err = opencc.New("s2t")
+		converter.openCC_Converter, err = opencc.New("s2t")
 	case T2S:
-		converter.converter, err = opencc.New("t2s")
+		converter.openCC_Converter, err = opencc.New("t2s")
 	case S2TW:
-		converter.converter, err = opencc.New("s2tw")
+		converter.openCC_Converter, err = opencc.New("s2tw")
 	case TW2S:
-		converter.converter, err = opencc.New("tw2s")
+		converter.openCC_Converter, err = opencc.New("tw2s")
 	case S2HK:
-		converter.converter, err = opencc.New("s2hk")
+		converter.openCC_Converter, err = opencc.New("s2hk")
 	case HK2S:
-		converter.converter, err = opencc.New("hk2s")
+		converter.openCC_Converter, err = opencc.New("hk2s")
 	case S2TWP:
-		converter.converter, err = opencc.New("s2twp")
+		converter.openCC_Converter, err = opencc.New("s2twp")
 	case TW2SP:
-		converter.converter, err = opencc.New("tw2sp")
+		converter.openCC_Converter, err = opencc.New("tw2sp")
 	case T2TW:
-		converter.converter, err = opencc.New("t2tw")
+		converter.openCC_Converter, err = opencc.New("t2tw")
 	case T2HK:
-		converter.converter, err = opencc.New("t2hk")
+		converter.openCC_Converter, err = opencc.New("t2hk")
 	default:
 		panic("Not implement this option")
 	}
@@ -53,6 +56,9 @@ func (converter *typicalConverter) Init(option Options, segmenter ...*gse.Segmen
 	}
 }
 
+// seg 分詞
+// text: 文字
+// 返回分詞後的結果
 func (converter *typicalConverter) seg(text string) []string {
 	if converter.segmenter == nil {
 		log.Fatal("Should run init() first!")
@@ -61,17 +67,20 @@ func (converter *typicalConverter) seg(text string) []string {
 	return converter.segmenter.Cut(text, true)
 }
 
+// justConvert 不分詞，直接轉換
+// text: 文字
+// 返回轉換後的結果
 func (converter *typicalConverter) justConvert(text string) (string, error) {
-	if converter.converter == nil {
+	if converter.openCC_Converter == nil {
 		log.Fatal("Should run init() first!")
 		panic("Should run init() first!")
 	}
-	return converter.converter.Convert(text)
+	return converter.openCC_Converter.Convert(text)
 }
 
 /* seg then convert */
 func (converter *typicalConverter) convert(text string) string {
-	if converter.segmenter == nil || converter.converter == nil {
+	if converter.segmenter == nil || converter.openCC_Converter == nil {
 		log.Fatal("Should run init() first!")
 		panic("Should run init() first!")
 	}
